bd3cae0fb2044bfef4d517d2437e780bf320fe0b
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-11-21 13:42:20 +0000

    Error handling for incorrect details entered

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -7,9 +7,10 @@ namespace RMDesktopUI.ViewModels
 {
     public class LoginViewModel : Screen
     {
+        private IAPIHelper _apiHelper;
         private string _userName;
         private string _password;
-        private IAPIHelper _apiHelper;
+        private string _errorMessage;
 
         public LoginViewModel(IAPIHelper apiHelper)
         {
@@ -18,12 +19,12 @@ namespace RMDesktopUI.ViewModels
 
         public string UserName
         {
-            get 
-            { 
-                return _userName; 
+            get
+            {
+                return _userName;
             }
-            set 
-            { 
+            set
+            {
                 _userName = value;
                 NotifyOfPropertyChange(() => UserName);
             }
@@ -31,17 +32,44 @@ namespace RMDesktopUI.ViewModels
 
         public string Password
         {
-            get 
-            { 
-                return _password; 
+            get
+            {
+                return _password;
             }
-            set 
-            { 
+            set
+            {
                 _password = value;
                 NotifyOfPropertyChange(() => Password);
                 NotifyOfPropertyChange(() => CanLogIn);
             }
         }
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
 
         public bool CanLogIn
         {
@@ -62,11 +90,12 @@ namespace RMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
     }
