53fdac3b5e8395a4a1d0f9c3ea72de7c92a72f41
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-11-26 16:30:00 +0000

    Product Data WIP

---
+++ RMDesktopUI.Library/Api/ProductEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.Library.Api
+{
+    public class ProductEndpoint
+    {
+        private IAPIHelper _apiHelper;
+
+        public ProductEndpoint(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async List<string> GetAll()
+        {
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/Product"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<LoggedInUserModel>();
+                    _loggedInUser.CreatedDate = result.CreatedDate;
+                    _loggedInUser.EmailAddress = result.EmailAddress;
+                    _loggedInUser.FirstName = result.FirstName;
+                    _loggedInUser.Id = result.Id;
+                    _loggedInUser.LastName = result.LastName;
+                    _loggedInUser.Token = token;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
