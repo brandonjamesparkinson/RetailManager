db8e72f007467ab8ab71637f20a64012136325e7
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-01-15 10:59:46 +0000

    + sale and inventory controller mods

---
+++ RMDataManager/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    //[Authorize]
+    public class InventoryController : ApiController
+    {
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(item);
+        }
+
+        public List<InventoryModel> GetSalesReport()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInventory();
+        }
+    }
+}
