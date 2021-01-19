d2472a2bc0782e83a211e719c2b44610d72c5248
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-01-19 16:04:48 +0000

    Added option to remove and add roles to users

--- RMDataManager/Controllers/UserController.cs
+++ RMDataManager/Controllers/UserController.cs
@@ -71,5 +71,34 @@ namespace RMDataManager.Controllers
             }
         }
 
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/AddRole")]
+        public void AddARole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.AddToRole(pairing.UserId, pairing.RoleName);
+
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/RemoveRole")]
+        public void RemoveARoll(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.RemoveFromRole(pairing.UserId, pairing.RoleName);
+
+            }
+        }
     }
 }
